ffd66ea81fe9d310154c22585f14ad461dcb4c10
AutomatedTester <dburns@mozilla.com>
2014-05-02 23:30:33 +0100

    start moving over the data into the view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,70 @@
+from flask import render_template
 from app import app
+import requests
+import datetime
+#import treestatus_stats
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Welcome To Futurama-Data because well its cool!!"
+    closure_months, closure_dates = main("mozilla-inbound")
+    c_data = [(datetime.datetime.strptime(k, "%Y-%m"), closure_months[k]) for k in sorted(closure_months.keys())]
+    import pdb; pdb.set_trace()
+    tot = c_data[1][1]['total'].total_seconds()
+    return render_template("index.html", total=tot)
+
+
+
+def main(tree):
+    response = requests.get('https://treestatus.mozilla.org/%s/logs?format=json&all=1' % tree, verify=False)
+    results = response.json()
+    delta = datetime.timedelta(0)
+    closed = None
+    closed_reason = None
+    dates = {}
+    month = {}
+    total = datetime.timedelta(0)
+    Added = None
+    for item in reversed(results['logs']):
+        if item['action'] == 'closed':
+            if closed is not None:
+                continue
+            closed = datetime.datetime.strptime(item['when'], "%Y-%m-%dT%H:%M:%S")
+            closed_reason = item['tags'][0] if len(item['tags']) > 0 else 'no reason'
+        elif item['action'] == 'open' or item['action'] == 'approval require':
+            if closed is None:
+                continue
+            opened = datetime.datetime.strptime(item['when'], "%Y-%m-%dT%H:%M:%S")
+            delta = opened - closed
+
+            if closed.date().isoformat() in dates:
+                try:
+                    dates[closed.date().isoformat()]['total'] = dates[closed.date().isoformat()]['total'] + delta
+                    dates[closed.date().isoformat()][closed_reason] = dates[closed.date().isoformat()][closed_reason] + delta
+                except:
+                    dates[closed.date().isoformat()][closed_reason] = delta
+            else:
+                dates[closed.date().isoformat()] = {'total': delta, closed_reason: delta}
+
+            year_month = "%s-%s" % (closed.date().year, closed.date().month if closed.date().month >= 10 else '0%s' % closed.date().month)
+
+            if year_month not in ['2012-06', '2012-07']:
+                if year_month in month:
+                    month[year_month]['total'] = month[year_month]['total'] + delta
+                    try:
+                        month[year_month][closed_reason] = month[year_month][closed_reason] + delta
+                    except:
+                        month[year_month][closed_reason] = delta
+                else:
+                    month[year_month] = {'total': delta, closed_reason: delta}
+
+                total += delta
+            closed = None
+            closed_reason = None
+        elif item['action'] == 'added':
+            Added = item['when']
+            print "Added on :%s" % item['when']
+
+    print "Tree has been closed for a total of %s since it was created on %s" % (total, Added)
+    #_print_dict(month)
+    return month, dates
